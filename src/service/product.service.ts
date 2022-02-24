fb36ae45968b91935fa74eb26455b3312cb5290f
Chinedu K. Eke <ekechinedu488@gmail.com>
2022-02-24 11:13:14 +0100

    added product service

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,7 +1,21 @@
-export const createProduct = async() => {}
+import { DocumentDefinition, FilterQuery, QueryOptions, UpdateQuery } from 'mongoose';
+import { Omit } from 'lodash';
+import ProductModel, { ProductDocument } from "../models/product.model";
 
-export const getProduct = async() => {}
+export const createProduct = async(input: DocumentDefinition<Omit<ProductDocument, 'createdAt' | 'updatedAt'>>) => {
+    return ProductModel.create(input)
+}
 
-export const updateProduct = async() => {}
+export const getProduct = async(query: FilterQuery<ProductDocument>, options: QueryOptions ={lean: true}) => {
+    return ProductModel.findOne(query, {}, options);
+}
 
-export const deleteProduct = async() => {}
+export const updateProduct = async(
+    query: FilterQuery<ProductDocument>, update: UpdateQuery<ProductDocument>, options: QueryOptions
+) => {
+    return ProductModel.updateOne(query, update, options);
+}
+
+export const deleteProduct = async(query: FilterQuery<ProductDocument>) => {
+    return ProductModel.deleteOne(query);
+}
