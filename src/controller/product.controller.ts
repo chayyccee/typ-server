a157638351bb2244af709f060bd1038f63a49cac
Chinedu K. Eke <ekechinedu488@gmail.com>
2022-02-24 12:30:19 +0100

    added product route

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,9 +1,65 @@
 import { Request, Response } from "express";
+import { createProductInput, deleteProductInput, getProductInput, updateProductInput } from "../schema/product.schema";
+import { createProduct, deleteProduct, getProduct, updateProduct } from "../service/product.service";
 
-export const createProductHandler = async (req: Request, res: Response) => {}
+export const createProductHandler = async (req: Request<{}, {}, createProductInput['body']>, res: Response) => {
+    const userId = res.locals.user._id;
 
-export const updateProdictHandler = async (req: Request, res: Response) => {}
+    const body = req.body;
 
-export const getProductHandler = async (req: Request, res: Response) => {}
+    const product = await createProduct({...body, user: userId});
 
-export const deleteProductHandler = async (req: Request, res: Response) => {}
+    return res.status(201).send(product);
+}
+
+export const updateProductHandler = async (req: Request<updateProductInput['params']>, res: Response) => {
+    const userId = res.locals.user._id;
+
+    const productId = req.params.productId;
+    const update = req.body;
+
+    const product = await getProduct({_id: productId, user: userId});
+
+    if (!product) {
+        return res.status(404).send('Product not found');
+    }
+
+    if (product.user.toString() !== userId.toString()) {
+        return res.status(403).send('Forbidden');
+    }
+
+    const updatedProduct = await updateProduct({_id: productId, user: userId}, update, {new: true});
+
+    return res.status(200).send(updatedProduct);
+}
+
+export const getProductHandler = async (req: Request<getProductInput['params']>, res: Response) => {
+    const productId = req.params.productId;
+    const product = await getProduct({productId});
+
+    if (!product) {
+        return res.status(404).send('Product not found');
+    }
+
+    return res.status(200).send(product);
+}
+
+export const deleteProductHandler = async (req: Request<deleteProductInput['params']>, res: Response) => {
+    const userId = res.locals.user._id;
+
+    const productId = req.params.productId;
+
+    const product = await getProduct({_id: productId, user: userId});
+
+    if (!product) {
+        return res.status(404).send('Product not found');
+    }
+
+    if (product.user.toString() !== userId.toString()) {
+        return res.status(403).send('Forbidden');
+    }
+
+    await deleteProduct({_id: productId});
+
+    return res.sendStatus(204);
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Request, Response } from 'express';
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from './controller/product.controller';
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionHandler } from './controller/session.controller';
 import { createUserHandler } from './controller/user.controller';
 import requireUser from './middleware/requireUser';
 import validate from './middleware/validateResource';
+import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from './schema/product.schema';
 import { createSessionSchema } from './schema/session.schema';
 import { createUserSchema } from './schema/user.schema';
 
@@ -12,9 +14,14 @@ const routes = (app: Express ) => {
     });
 
     app.post('/api/users', validate(createUserSchema), createUserHandler);
-    app.post('/api/sessions', validate(createSessionSchema), createUserSessionHandler);
+    app.post('/api/sessions', validate(createSessionSchema), createUserSessionHandler); // to login
     app.get('/api/sessions', requireUser, getUserSessionHandler);
     app.delete('/api/sessions', requireUser, deleteSessionHandler);
+
+    app.post('api/products', [requireUser, validate(createProductSchema)], createProductHandler);
+    app.put('api/products', [requireUser, validate(updateProductSchema)], updateProductHandler);
+    app.get('api/products', [requireUser, validate(getProductSchema)], getProductHandler);
+    app.delete('api/products', [requireUser, validate(deleteProductSchema)], deleteProductHandler);
 };
 
 export default routes;
