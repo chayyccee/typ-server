5221a46f54a48fcaf33bdc774551dc22edb1e031
Chinedu K. Eke <ekechinedu488@gmail.com>
2022-02-24 11:31:47 +0100

    added product schema

---
+++ src/controller/product.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const createProductHandler = async (req: Request, res: Response) => {}
+
+export const updateProdictHandler = async (req: Request, res: Response) => {}
+
+export const getProductHandler = async (req: Request, res: Response) => {}
+
+export const deleteProductHandler = async (req: Request, res: Response) => {}
